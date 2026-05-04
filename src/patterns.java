1ba452444d7cfd72c629c0822a27313fbdf56729
SatyamRaghuwanshii <satyamraghuwanshi71@gmail.com>
2026-05-04 19:43:33 +0530

    filped triangle pattern

--- src/patterns.java
+++ src/patterns.java
@@ -151,5 +151,14 @@ public void pattern10(int n){
 }
 
 public void pattern11(int n){
-
+    for(int i = 1; i <= n; i++){
+        for(int k = 0; k < n-i; k++){
+            System.out.print("  ");
+        }
+        for(int j = 0; j < i; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    System.out.println();
 }
